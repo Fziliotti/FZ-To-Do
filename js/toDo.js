60a3c5c7e33078f8a75f15b2ad56548c74742ce4
Fabricio Fernandes Ziliotti <ffz.ziliotti@gmail.com>
2018-10-01 09:12:42 -0300

    Colocando animacao header

--- js/toDo.js
+++ js/toDo.js
@@ -5,9 +5,9 @@ var btnElement = document.querySelector('#app button');
 var todos = JSON.parse(localStorage.getItem('lista_tarefas')) || [];
 
 
-function renderTodos(){
+function renderTodos() {
 	listElement.innerHTML = ''; //limpa a lista antes de renderizar
-	for (todo of todos){
+	for (todo of todos) {
 		var todoElement = document.createElement('li');
 		todoElement.setAttribute('class', "todo-item my-4 list-group-item");
 
@@ -17,7 +17,7 @@ function renderTodos(){
 		linkElement.setAttribute('class', 'btn btn-danger ml-2 float-right')
 		var linkText = document.createTextNode('DEL');
 		var pos = todos.indexOf(todo);
-		linkElement.setAttribute('onclick', 'deleteTodo('+ pos+')');
+		linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
 
 		// CRIANDO O RELACIONAMENTO DOS ELEMENTOS
 		linkElement.appendChild(linkText);
@@ -28,29 +28,29 @@ function renderTodos(){
 }
 renderTodos(); // RENDERIZAR PELOMENOS UMA VEZ
 
-function addTodo(){
+function addTodo() {
 	var todoText = inputElement.value;
-	if (todoText.trim()){
+	if (todoText.trim()) {
 		swal({
 			position: 'top-end',
 			type: 'success',
 			title: 'Inserido com sucesso!',
 			showConfirmButton: false,
 			timer: 1000
-		  })
+		})
 		todos.push(todoText);
 		inputElement.value = '';
 		renderTodos();
 		saveToStorage();
-	}else{
+	} else {
 		swal({
 			position: 'top-end',
 			type: 'error',
 			title: 'Digite alguma coisa no campo de texto',
 			showConfirmButton: false,
 			timer: 1000
-		  })
-		  todoText.focus();
+		})
+		todoText.focus();
 	}
 };
 
@@ -58,23 +58,23 @@ function addTodo(){
 
 
 
-inputElement.addEventListener('keyup', function(e){
-  var key = e.which || e.keyCode;
-  if (key == 13) { 
-  	addTodo();
-  }
+inputElement.addEventListener('keyup', function (e) {
+	var key = e.which || e.keyCode;
+	if (key == 13) {
+		addTodo();
+	}
 });
 
 btnElement.onclick = addTodo;
 
-function deleteTodo(pos){
+function deleteTodo(pos) {
 	const swalWithBootstrapButtons = swal.mixin({
 		confirmButtonClass: 'btn btn-success',
 		cancelButtonClass: 'btn btn-danger',
 		buttonsStyling: false,
-	  })
-	  
-	  swalWithBootstrapButtons({
+	})
+
+	swalWithBootstrapButtons({
 		title: 'Tem certeza disso?',
 		text: "Não poderá ser revertido depois!",
 		type: 'warning',
@@ -82,44 +82,45 @@ function deleteTodo(pos){
 		confirmButtonText: 'Simm',
 		cancelButtonText: 'Cancelar',
 		reverseButtons: true
-	  }).then((result) => {
+	}).then((result) => {
 		if (result.value) {
-		  swalWithBootstrapButtons(
-			'Deletado!',
-			'Sua tarefa já era!',
-			'success'
-		  )
-		  todos.splice(pos,1);
-		  renderTodos();
-		  saveToStorage();
-		 
+			swalWithBootstrapButtons(
+				'Deletado!',
+				'Sua tarefa já era!',
+				'success'
+			)
+			todos.splice(pos, 1);
+			renderTodos();
+			saveToStorage();
+
 		} else if (
-		  // Read more about handling dismissals
-		  result.dismiss === swal.DismissReason.cancel
+			// Read more about handling dismissals
+			result.dismiss === swal.DismissReason.cancel
 		) {
-		  swalWithBootstrapButtons(
-			'Cancelado',
-			'Sua tarefa tá sussa! :)',
-			'error'
-		  )
+			swalWithBootstrapButtons(
+				'Cancelado',
+				'Sua tarefa tá sussa! :)',
+				'error'
+			)
 		}
-	  })
+	})
 }
 
 
-function marcarTarefa(obj){
-	 obj.classList.toggle("bg-success");
+function marcarTarefa(obj) {
+	obj.classList.toggle("bg-success");
 }
 
 body = document.querySelector("body");
 app = document.getElementById("app");
-function mudarTema(){
+
+function mudarTema() {
 	body.classList.toggle("tema-light");
 	app.classList.toggle("text-dark");
 }
 
 
 
-function saveToStorage(){
+function saveToStorage() {
 	localStorage.setItem('lista_tarefas', JSON.stringify(todos));
 }
